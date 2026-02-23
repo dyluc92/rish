081f77c89f076c1d40ac6a0a478cabe7ab646664
dyluc92 <bolaakunempat4@gmail.com>
2026-02-23 13:37:13 +0530

    Update [Rish-Gaming]Township37-fix.lua

--- [Rish-Gaming]Township37-fix.lua
+++ [Rish-Gaming]Township37-fix.lua
@@ -17395,7 +17395,7 @@ function ipen1()
             flags = gg.TYPE_DWORD
         }
         checkList[#checkList + 1] = {
-            address = v.address - 0xB8,
+            address = v.address + 0xB8,
             flags = gg.TYPE_DWORD
         }
     end
@@ -17413,7 +17413,7 @@ function ipen1()
 
         if val1 == 65537 and val2 == 2 then
             valid1[#valid1 + 1] = {
-                address = v.address - 0xB4,
+                address = v.address + 0xB4,
                 flags = gg.TYPE_DWORD,
                 value = val
             }
